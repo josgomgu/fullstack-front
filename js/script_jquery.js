f378441804a31b4d281cbfc2190e34a41485b5c9
josgomgu <jgomez@mashumanoconsultores.com>
2021-12-22 17:51:35 -0500

    Incorporación checksize

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -25,3 +25,25 @@ function setColor(p_componente,p_color)
     $('#'+lblapellido).css("color", p_color);
 }
 
+
+$("#tamano").on('change', function() {
+
+    //Se obtiene codigo de llamado desde las ayudas de postman
+    let form = new FormData();
+	form.append("tamano", this.value);
+
+	let settings = {
+	  "url": "http://localhost:5000/checksize",
+	  "method": "POST",
+	  "timeout": 0,
+	  "processData": false,
+	  "mimeType": "multipart/form-data",
+	  "contentType": false,
+	  "data": form
+	};
+
+	$.ajax(settings).done(function (response) {	  
+	  $("#resultado_tamano").text(response);
+	});
+});
+
